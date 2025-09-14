6cc1f9f42d5af761abe1828b181c706121f4bde7
caleb <calebdjones@gmail.com>
2025-09-14 15:39:22 -0400

    remove unused test config

--- setup.py
+++ setup.py
@@ -1,4 +1,3 @@
-from __future__ import print_function
 import codecs
 import sys
 import platform
@@ -19,11 +18,6 @@ ext = Extension('pyasn.pyasn_radix',
 
 extra_kwargs = {'ext_modules': [ext]}
 reqs = []
-if sys.version_info[0] == 2 and sys.version_info[1] < 7:
-    reqs.append('ordereddict')
-if sys.version_info[0] == 2:
-    reqs.append('backport-ipaddress')
-
 
 utils = glob.glob('pyasn-utils/*.py')
 
